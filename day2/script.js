7bf7bcec98c906abf4445bca4921ac7ed75c154d
BalajiPonnamanda1 <balajiponnamanda80@gmail.com>
2026-04-11 09:52:09 +0530

    changes done

--- day2/script.js
+++ day2/script.js
@@ -7,3 +7,15 @@ for(let i = 0; i < arr.length; i++)
 console.log(arr);
 
 let val=[10,20,"hello",true]
+// adding data in last
+val.push('cvcorp')
+// removng in last 
+val.pop()
+
+// adding in front
+val.unshift("Raju")
+
+// removing in first
+val.shift()
+
+
