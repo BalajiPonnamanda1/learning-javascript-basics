626049955408254e9a7003f08b37d48b40f5ec0a
BalajiPonnamanda1 <balajiponnamanda80@gmail.com>
2026-04-13 09:12:25 +0530

    changes done

--- day2/Objects/obj.js
+++ day2/Objects/obj.js
@@ -1,10 +1,28 @@
-const obj={
-    id:1,
-    name:"guest",
-    arr:[1,true,'hai',23.4]
-}
-console.log(obj.loc);
-obj.loc='hyd';
-console.log(obj.loc);
-console.log(obj.arr);
+const users=[
+    {
+        id:1,
+        name:'Raju',
+        loc:'eluru'
+    },{
+        id:2,
+        name:'balaji',
+        loc:'palakollu'
+    }
+]
+console.log(users[0].name);
 
+for (const element of users) {
+    if(element.name==='Raju')
+    {
+        console.log("found");
+        break;
+    }
+}
+
+// for (const key in object) {
+//     if (!Object.hasOwn(object, key)) continue;
+    
+//     const element = object[key];
+    
+    
+// }
