27145fb95d4c1118246b0ad8b9b7092fbed119c8
BalajiPonnamanda1 <balajiponnamanda80@gmail.com>
2026-04-14 08:56:30 +0530

    day3 added

---
+++ day3/literal.js
@@ -0,0 +1,10 @@
+let name = 'Balaji'
+let age = '23'
+console.log(`my name is ${name}. my age is ${age}`);
+
+// function expression
+const val = function(a, b){
+    console.log(a + b);
+}
+
+val(2,3);
