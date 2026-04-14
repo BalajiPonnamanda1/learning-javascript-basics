fe95836a2c22838de3e5c982cdcf3ca979205ae8
BalajiPonnamanda1 <balajiponnamanda80@gmail.com>
2026-04-14 09:47:11 +0530

    dom added

---
+++ dom/dom.js
@@ -0,0 +1,7 @@
+let boxes = document.getElementsByClassName("box")
+let para = document.getElementsByTagName("para")
+console.log(boxes);
+console.log(para);
+boxes[1].innerHTML = '<b> hello guru </b>'
+para[0].innerHTML = '<b> haii </b>'
+
