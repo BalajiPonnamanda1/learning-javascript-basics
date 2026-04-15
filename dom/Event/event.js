c2993438e38f071df8f2d416cfb0aead649fa0f9
BalajiPonnamanda1 <balajiponnamanda80@gmail.com>
2026-04-15 09:11:37 +0530

    changes done

--- dom/Event/event.js
+++ dom/Event/event.js
@@ -1,7 +1,16 @@
 function clicks(){
     let x = document.getElementById("id")
     x.innerText = x.innerText == 'hello' ? 'cvcorp' : 'hello';
-    x.style.color = x.style.color == 'black' ? 'red' : 'black';
+    // x.style.color = x.style.color == 'black' ? 'red' : 'black';
+    if(x.style.color == 'black')
+    {
+        x.style.color = 'red'
+        x.style.background = 'black'
+    }
+    else {
+        x.style.color = 'black'
+        x.style.background = 'red'
+    }
     console.log("hello")
 }
 console.log("hai");
