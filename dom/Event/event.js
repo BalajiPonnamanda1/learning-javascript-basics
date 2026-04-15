6b26659452c14557be0eec8272d45b472ec400ac
BalajiPonnamanda1 <balajiponnamanda80@gmail.com>
2026-04-15 08:59:11 +0530

    click fun added

---
+++ dom/Event/event.js
@@ -0,0 +1,7 @@
+function clicks(){
+    let x = document.getElementById("id")
+    x.innerText = x.innerText == 'hello' ? 'cvcorp' : 'hello';
+    x.style.color = x.style.color == 'black' ? 'red' : 'black';
+    console.log("hello")
+}
+console.log("hai");
