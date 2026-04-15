e804b82a1b1222a320b8f6c6ddaf5bfa090f1238
BalajiPonnamanda1 <balajiponnamanda80@gmail.com>
2026-04-15 22:34:08 +0530

    changes done

--- dom/Event/event.js
+++ dom/Event/event.js
@@ -1,16 +1,27 @@
-function clicks(){
-    let x = document.getElementById("id")
-    x.innerText = x.innerText == 'hello' ? 'cvcorp' : 'hello';
-    // x.style.color = x.style.color == 'black' ? 'red' : 'black';
-    if(x.style.color == 'black')
-    {
-        x.style.color = 'red'
-        x.style.background = 'black'
+let status = true;
+function clicks() {
+    //     let x = document.getElementById("id")
+    //    x.innerText = x.innerText == 'hello' ? 'cvcorp' : 'hello';
+    //     x.style.color = x.style.color == 'black' ? 'red' : 'black';
+    //     if(x.style.color == 'black')
+    //     {
+    //         x.style.color = 'red'
+    //         x.style.background = 'black'
+    //     }
+    //     else {
+    //         x.style.color = 'black'
+    //         x.style.background = 'red'
+    //     }
+    let x = document.getElementById("ids")
+
+    if (status) {
+        x.src = "savitri.jpg"
+        status = false;
     }
     else {
-        x.style.color = 'black'
-        x.style.background = 'red'
+        x.src = "Anchor1.jpg";
+        status = true;
     }
-    console.log("hello")
+
 }
-console.log("hai");
+
